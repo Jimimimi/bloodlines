bb2d48f70695fbcc6fa274ecb0ce66aa871f8f54
Jimimimi <jimmy.messinis@gmail.com>
2014-06-26 03:46:50 +0300

    Create outline.js

---
+++ outline.js
@@ -0,0 +1,30 @@
+function Person (data) = {
+    this.name = data.name;
+    this.surname = data.surname,
+    this.desc = data.desc,
+    this.fullname = function(){
+        return this.name + ' ' + this.surname;
+    },
+    this.influence = {
+        political : 0,
+        economical: 0,
+        social:0
+    }
+}
+
+function Family (data) = {
+    this.name = data.name;
+    this.desc = data.desc;
+    this.members = [],
+    this.currentHead = '',
+    tjos.addMember = function(person){
+        this.members.push(person);
+    };
+    this.influence = function(t){
+        var inf = 0;
+        this.members.forEach(function(m){
+            inf += m.influence[t];
+        });
+        return inf;
+    }
+}
